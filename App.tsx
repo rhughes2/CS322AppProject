c20c5a1567dd2aab9795a249739de6051dfc3f9c
RJ <rhughes2@luc.edu>
2024-10-11 13:56:18 -0500

    Complete ReWrite of Homescreen

--- App.tsx
+++ App.tsx
@@ -5,6 +5,8 @@
  * @format
  */
 
+/* !!! THIS IS THE OLD VERSION JUST FOR SAFE KEEPING !!! 
+
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
@@ -83,9 +85,7 @@ function App(): React.JSX.Element {
             source={require('./assets/bell-button.png')}>
           </Image>
           </Section>
-          {/* Will most likely not want to use section element here but not sure what to replace with, just dont want the title ya feel? -RJ */}
           <Section title="">
-            {/* Will change to more than just burgers later lol -RJ*/}
             <Image
               style={styles.button}
               source={require('./assets/burger-button.png')}
@@ -114,7 +114,6 @@ function App(): React.JSX.Element {
           <Section title="Featured Bars">
           <Image
               style={styles.bar}
-              // Isn't it brew pub not brew hub? or maybe I just haven't been to this one :p -RJ
               source={require('./assets/brew-hub.png')}
               />
           </Section>
@@ -152,7 +151,6 @@ const styles = StyleSheet.create({
     fontWeight: '700',
   },
 
-  // These were mostly ripped from the Figma mockup, please edit as you see fit -RJ
 
   bell: {
     width: 51,
@@ -195,4 +193,167 @@ const styles = StyleSheet.create({
 
 });
 
-export default App;
+export default App; */
+
+import React from 'react';
+import { View, Text, TextInput, ScrollView, FlatList, Image, TouchableOpacity, StyleSheet } from 'react-native';
+
+// Hey look emojis! I'm sure we can find a way to make these the nicer logos but now I think its fun lol -RJ
+
+const HomeScreen = () => {
+  const categories = [
+    { id: '1', title: 'Beer', icon: '🍺' },
+    { id: '2', title: 'Wines', icon: '🍷' },
+    { id: '3', title: 'Cocktails', icon: '🍹' },
+    { id: '4', title: 'Burgers', icon: '🍔' },
+    { id: '5', title: 'Promos', icon: '🏷️' },
+  ];
+
+  // For some reason the images don't populate... I've tried a bunch of stuff
+
+  const featuredBars = [
+    { id: '1', title: 'Istmo Brew Hub', rating: '4.5', image: './Drinkea Images/Drinks/Drink.png', time: '10-15 mins' },
+    { id: '2', title: 'Feroz', rating: '4.7', image: './Drinkea Images/Drinks/two-cocktails-by-restaurant-open-fire-royalty-free-image-1680877011.jpg', time: '15-20 mins' },
+  ];
+
+  return (
+    <ScrollView style={styles.container}>
+      {/* Header Section */}
+      <View style={styles.header}>
+        <Text style={styles.title}>What's the plan for the night?</Text>
+        <View style={styles.searchBar}>
+          <TextInput style={styles.searchInput} placeholder="Look for food or bar..." />
+          <TouchableOpacity style={styles.bellIcon}>
+            <Text>🔔</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      {/* Categories */}
+      <ScrollView horizontal showsHorizontalScrollIndicator={false} style={styles.categories}>
+        {categories.map((category) => (
+          <View key={category.id} style={styles.categoryItem}>
+            <Text style={styles.categoryIcon}>{category.icon}</Text>
+            <Text>{category.title}</Text>
+          </View>
+        ))}
+      </ScrollView>
+
+      {/* Featured Bars */}
+      <Text style={styles.sectionTitle}>Featured Bars</Text>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false} style={styles.featuredBars}>
+        {featuredBars.map((bar) => (
+          <View key={bar.id} style={styles.barCard}>
+            <Image source={{ uri: bar.image }} style={styles.barImage} />
+            <Text>{bar.title}</Text>
+            <Text>{bar.rating} ⭐</Text>
+            <Text>{bar.time}</Text>
+          </View>
+        ))}
+      </ScrollView>
+
+      {/* Popular Places */}
+      <Text style={styles.sectionTitle}>Popular Places</Text>
+      <FlatList
+        data={featuredBars} // If we get non-demo data it would go here -RJ
+        numColumns={2}
+        renderItem={({ item }) => (
+          <View style={styles.popularPlaceCard}>
+            <Image source={{ uri: item.image }} style={styles.popularImage} />
+            <Text>{item.title}</Text>
+            <Text>{item.rating} ⭐</Text>
+          </View>
+        )}
+        keyExtractor={(item) => item.id}
+      />
+
+      {/* Bottom Navigation (I'm not sure this works at all lol -RJ*/}
+      <View style={styles.bottomNav}>
+        <TouchableOpacity><Text>🔍</Text></TouchableOpacity>
+        <TouchableOpacity><Text>📍</Text></TouchableOpacity>
+        <TouchableOpacity><Text>🏠</Text></TouchableOpacity>
+        <TouchableOpacity><Text>👤</Text></TouchableOpacity>
+        <TouchableOpacity><Text>☰</Text></TouchableOpacity>
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  header: {
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  searchBar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  searchInput: {
+    flex: 1,
+    backgroundColor: '#f0f0f0',
+    padding: 10,
+    borderRadius: 8,
+  },
+  bellIcon: {
+    marginLeft: 10,
+  },
+  categories: {
+    padding: 10,
+    flexDirection: 'row',
+  },
+  categoryItem: {
+    alignItems: 'center',
+    marginHorizontal: 10,
+  },
+  categoryIcon: {
+    fontSize: 24,
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginVertical: 10,
+    marginLeft: 10,
+  },
+  featuredBars: {
+    paddingLeft: 10,
+  },
+  barCard: {
+    marginRight: 10,
+    width: 150,
+    backgroundColor: '#f9f9f9',
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+  barImage: {
+    width: '100%',
+    height: 100,
+  },
+  popularPlaceCard: {
+    flex: 1,
+    margin: 5,
+    backgroundColor: '#f9f9f9',
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+  popularImage: {
+    width: '100%',
+    height: 100,
+  },
+  bottomNav: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingVertical: 10,
+    borderTopWidth: 1,
+    borderColor: '#e0e0e0',
+  },
+});
+
+export default HomeScreen;
